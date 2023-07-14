ea020beed8cf725b640982ae433e8aee679fef0c
Cindy <sunshinec.lu@gmail.com>
2023-07-14 09:41:02 -0500

    Initial Commit

---
+++ finalProjectApp/finalProjectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectAppApp.swift
+//  finalProjectApp
+//
+//  Created by Cindy Lu on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
