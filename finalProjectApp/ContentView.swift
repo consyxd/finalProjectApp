ac1dd5472840eadc9d67bc43fda67a05422763f7
Marina <hernandez.r.marina448@gmail.com>
2023-07-17 11:35:08 -0700

    add

--- finalProjectApp/ContentView.swift
+++ finalProjectApp/ContentView.swift
@@ -8,19 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, everyone!!")
+        
+        ZStack {
+            NavigationStack{
+                VStack {
+                    Text(textTitle)
+                        .font(.title)
+                    TextField("Type Name Here...", text: $name)
+                        .multilineTextAlignment(.center)
+                        .font(.title)
+                        .border(Color.gray, width: 1)
+                    Button("Submit Name"){
+                        textTitle = "Welcome, \(name) <3"
+                    }
+                    .foregroundColor(.white)
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(.black)
+                    
+                }.padding()
+            }
         }
-        .padding()
+    }
+            
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
