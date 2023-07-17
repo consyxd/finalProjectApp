630d2019f2f0e02b48a6a280ea432874bcc763a7
Marina <hernandez.r.marina448@gmail.com>
2023-07-17 11:38:12 -0700

    a

---
+++ finalProjectApp/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  finalProjectApp
+//
+//  Created by Scholar on 7/17/23.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
